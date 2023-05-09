1678e45931868fe784daa23102f275b4be577e60
kyaw swar htet <kyawswarhtet101094@gmail.com>
2023-05-09 17:11:00 +0630

    add typescript to project ksh

---
+++ src/model/userModel.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose"
+const { Schema } = mongoose;
+const UserSchema = new Schema(
+    {
+        
+        username: {
+            type: String,
+            require: true
+        },
+        email: {
+            type: String,
+            require:true
+        },
+         password: {
+            type: String,
+            require:true
+        },
+          login: {
+            type: Boolean,
+            require: true,
+            default: false
+        },
+           gender: {
+      type: String,
+      default: "",
+    },
+           address: {
+               type: String,
+            default:""
+        },
+           profilePicture: {
+               type:[],
+               require: true,
+            default:null
+        },
+         
+    }, {
+        timestamps:true
+    }
+)
+const UserModel = mongoose.model("user", UserSchema)
+export default UserModel
